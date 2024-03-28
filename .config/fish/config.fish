26f8108b1473fc86e9d5455f301664b79cee380c
alidevs <alialatyah2@gmail.com>
2024-03-28 21:42:40 +0300

    added fish aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,20 @@ end
 starship init fish | source
 direnv hook fish | source
 pyenv init - | source
+neofetch
+status is-login; and source (jenv init -|psub)
+zoxide init fish | source
+
+# Aliases
+alias ls=lsd
+alias ll="ls -l"
+alias la="ls -a"
+alias lla="ls -la"
+alias l="ls -l"
+alias lt="ls --tree"
+alias lat="ls -la --tree"
+
+alias cat=bat
+
+alias cd="z"
+alias lg="lazygit"
