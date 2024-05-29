56b7687878c176b188a82ce8cc8d0511140dabd3
alidevs <alialatyah2@gmail.com>
2024-05-30 02:46:21 +0300

    replace rust plugins with rustaceanvim

--- .config/nvim/lua/plugins/configs/lspconfig.lua
+++ .config/nvim/lua/plugins/configs/lspconfig.lua
@@ -11,23 +11,6 @@ lspconfig.pyright.setup {
   root_dir = lspconfig.util.root_pattern(".venv", "docker-compose.yml", ".direnv"),
 }
 
-lspconfig.rust_analyzer.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "rust" },
-  root_dir = lspconfig.util.root_pattern "Cargo.toml",
-  settings = {
-    ["rust-analyzer"] = {
-      cargo = {
-        loadOutDirsFromCheck = true,
-      },
-      procMacro = {
-        enable = true,
-      },
-    },
-  },
-}
-
 local function organize_imports()
   local params = {
     command = "_typescript.organizeImports",
--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -12,15 +12,28 @@ local plugins = {
 
   -- Rust Support
   {
-    "rust-lang/rust.vim",
-    ft = { "rust" },
-  },
-  {
     "mrcjkb/rustaceanvim",
     version = "^4",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "mfussenegger/nvim-dap",
+      {
+        "lvimuser/lsp-inlayhints.nvim",
+        opts = {},
+      },
+    },
     ft = { "rust" },
     init = function()
-      vim.g.rustaceanvim = {}
+      vim.g.rustaceanvim = {
+        inlay_hints = {
+          highlight = "NonText",
+        },
+        server = {
+          on_attach = function(client, bufnr)
+            require("lsp-inlayhints").on_attach(client, bufnr)
+          end,
+        },
+      }
     end,
   },
 
