bc2d2707bafa9530b3e6c862522a8b8e76598c9a
alidevs <alialatyah2@gmail.com>
2024-06-11 11:46:33 +0300

    fix oldfiles going normal mode by default

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -10,7 +10,7 @@ map("n", "<leader>nd", "<cmd>NoiceDismiss<cr>", { desc = "Dismiss noice" })
 
 -- Telescope
 map("n", "<leader><leader>", "<cmd>Telescope frecency workspace=CWD<cr>", { desc = "Frecent files" })
-map("n", "<leader><Tab>", "<cmd>Telescope oldfiles cwd_only=true<cr><ESC>", { desc = "Open recent files" })
+map("n", "<leader><Tab>", "<cmd>Telescope oldfiles cwd_only=true<cr>", { desc = "Open recent files" })
 map("n", "<leader>fy", "<cmd>Telescope lsp_document_symbols<cr>", { desc = "Document symbols" })
 map("n", "<leader>fs", "<cmd>Telescope grep_string<cr>", { desc = "Search for string" })
 map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<cr>", { desc = "Fuzzy find in buffer" })
