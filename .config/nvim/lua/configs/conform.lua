b24f0a3003338c71211bc73cb14758ffaf6793ff
alidevs <alialatyah2@gmail.com>
2024-09-06 02:45:23 +0300

    add json formatter

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -7,6 +7,7 @@ local options = {
     rust = { "rustfmt" },
     ruby = { "rufo" },
     yaml = { "yamlfix" },
+    json = { "fixjson" },
     python = function(bufnr)
       if require("conform").get_formatter_info("ruff_format", bufnr).available then
         return { "ruff_fix", "ruff_format", "ruff_organize_imports" }
