bf2328ff0589141c5f11a018c5b55540225276b2
alidevs <alialatyah2@gmail.com>
2024-09-15 00:40:30 +0300

    replace deprecated tsserver with ts_ls

--- .config/nvim/lua/plugins/configs/lspconfig.lua
+++ .config/nvim/lua/plugins/configs/lspconfig.lua
@@ -19,7 +19,7 @@ local function organize_imports()
   vim.lsp.buf.execute_command(params)
 end
 
-lspconfig.tsserver.setup {
+lspconfig.ts_ls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   init_options = {
