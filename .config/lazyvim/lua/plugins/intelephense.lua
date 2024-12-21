f3593dde71014a643976600c343f7ddda68cfcb9
alidevs <alialatyah2@gmail.com>
2024-12-22 02:04:43 +0300

    laravel plugins

---
+++ .config/lazyvim/lua/plugins/intelephense.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts.servers = vim.tbl_deep_extend("force", opts.servers or {}, {
+        intelephense = {
+          filetypes = { "php", "blade", "php_only" },
+          settings = {
+            intelephense = {
+              filetypes = { "php", "blade", "php_only" },
+              files = {
+                associations = { "*.php", "*.blade.php" },
+                maxSize = 5000000,
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
+}
---
+++ .config/lazyvim/lua/plugins/laravel.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "adalessa/laravel.nvim",
+    dependencies = {
+      "tpope/vim-dotenv",
+      "MunifTanjim/nui.nvim",
+      "kevinhwang91/promise-async",
+    },
+    cmd = { "Laravel" },
+    event = { "VeryLazy" },
+    opts = {},
+    config = true,
+  },
+}
