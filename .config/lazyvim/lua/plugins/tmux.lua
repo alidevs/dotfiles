9ae6ff1594ce3e1a3a20d2f354aef9c90fa20b53
alidevs <alialatyah2@gmail.com>
2024-12-23 07:29:11 +0300

    fix clipboard sync

--- .config/lazyvim/lua/plugins/tmux.lua
+++ .config/lazyvim/lua/plugins/tmux.lua
@@ -2,7 +2,11 @@ return {
   {
     "aserowy/tmux.nvim",
     config = function()
-      return require("tmux").setup()
+      return require("tmux").setup({
+        copy_sync = {
+          enable = false,
+        }
+      })
     end,
     event = "VeryLazy",
   },
