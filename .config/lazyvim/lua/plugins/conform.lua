a7775753c93ed38bc26595179369851155ad1829
alidevs <alialatyah2@gmail.com>
2025-01-08 12:58:30 +0300

    yamlfix configs

--- .config/lazyvim/lua/plugins/conform.lua
+++ .config/lazyvim/lua/plugins/conform.lua
@@ -6,13 +6,14 @@ return {
     local opts = {
       default_format_opts = {
         timeout_ms = 3000,
-        async = false, -- not recommended to change
-        quiet = false, -- not recommended to change
+        async = false,           -- not recommended to change
+        quiet = false,           -- not recommended to change
         lsp_format = "fallback", -- not recommended to change
       },
       formatters_by_ft = {
         php = { "pint" },
         blade = { "blade-formatter", "rustywind" },
+        yaml = { "yamlfix" },
       },
       -- LazyVim will merge the options you set here with builtin formatters.
       -- You can also define any custom formatters here.
@@ -22,7 +23,8 @@ return {
         pint = {
           meta = {
             url = "https://github.com/laravel/pint",
-            description = "Laravel Pint is an opinionated PHP code style fixer for minimalists. Pint is built on top of PHP-CS-Fixer and makes it simple to ensure that your code style stays clean and consistent.",
+            description =
+            "Laravel Pint is an opinionated PHP code style fixer for minimalists. Pint is built on top of PHP-CS-Fixer and makes it simple to ensure that your code style stays clean and consistent.",
           },
           command = util.find_executable({
             vim.fn.stdpath("data") .. "/mason/bin/pint",
@@ -31,6 +33,14 @@ return {
           args = { "$FILENAME" },
           stdin = false,
         },
+        yamlfix = function()
+          return {
+            env = {
+              YAMLFIX_SEQUENCE_STYLE = "block_style",
+              YAMLFIX_WHITELINES = "1",
+            },
+          }
+        end,
       },
     }
     return opts
