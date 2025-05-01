1862112280230929f50059de42936cb06ce35630
alidevs <alialatyah2@gmail.com>
2025-05-01 11:14:12 +0300

    smarter php formatter

--- .config/lazyvim/lua/plugins/conform.lua
+++ .config/lazyvim/lua/plugins/conform.lua
@@ -1,4 +1,5 @@
 local util = require("conform.util")
+
 return {
   "stevearc/conform.nvim",
   opts = function()
@@ -6,12 +7,29 @@ return {
     local opts = {
       default_format_opts = {
         timeout_ms = 3000,
-        async = false,           -- not recommended to change
-        quiet = false,           -- not recommended to change
-        lsp_format = "fallback", -- not recommended to change
+        async = false,
+        quiet = false,
+        lsp_format = "fallback",
       },
       formatters_by_ft = {
-        php = { "pint" },
+        php = function(bufnr)
+          local filename = vim.api.nvim_buf_get_name(bufnr)
+          local dir = vim.fs.dirname(filename)
+
+          local found = vim.fs.find(function(name)
+            return name == ".php-cs-fixer.php" or name == ".php-cs-fixer.dist.php"
+          end, {
+            upward = true,
+            path = dir,
+            type = "file",
+          })
+
+          if found and #found > 0 then
+            return { "php" }
+          else
+            return { "pint" }
+          end
+        end,
         blade = { "blade-formatter", "rustywind" },
         yaml = { "yamlfix" },
         python = {
@@ -28,18 +46,46 @@ return {
         html = { "prettier" },
         go = { "gofmt" },
       },
-      -- LazyVim will merge the options you set here with builtin formatters.
-      -- You can also define any custom formatters here.
-      ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
       formatters = {
         injected = { options = { ignore_errors = true } },
+        php = function(bufnr)
+          local filename = vim.api.nvim_buf_get_name(bufnr)
+          local dir = vim.fs.dirname(filename)
+
+          local found = vim.fs.find(function(name)
+            return name == ".php-cs-fixer.php" or name == ".php-cs-fixer.dist.php"
+          end, {
+            upward = true,
+            path = dir,
+            type = "file",
+          })
+
+          if found and #found > 0 then
+            vim.notify("Using php-cs-fixer: " .. found[1])
+            return {
+              command = util.find_executable({
+                "vendor/bin/php-cs-fixer",
+                vim.fn.stdpath("data") .. "/mason/bin/php-cs-fixer",
+                "php-cs-fixer",
+              }, "php-cs-fixer"),
+              args = { "fix", "$FILENAME" },
+              stdin = false,
+            }
+          else
+            vim.notify("Falling back to pint")
+            return {
+              command = util.find_executable({
+                "vendor/bin/pint",
+                vim.fn.stdpath("data") .. "/mason/bin/pint",
+                "pint",
+              }, "pint"),
+              args = { "$FILENAME" },
+              stdin = false,
+            }
+          end
+        end,
         pint = {
-          meta = {
-            url = "https://github.com/laravel/pint",
-            description =
-            "Laravel Pint is an opinionated PHP code style fixer for minimalists. Pint is built on top of PHP-CS-Fixer and makes it simple to ensure that your code style stays clean and consistent.",
-          },
-          command = util.find_executable({
+          command = require("conform.util").find_executable({
             vim.fn.stdpath("data") .. "/mason/bin/pint",
             "vendor/bin/pint",
           }, "pint"),
