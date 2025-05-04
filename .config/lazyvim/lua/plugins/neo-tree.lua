ebc171d425d3517389ae4e5a0030c7979e348754
alidevs <alialatyah2@gmail.com>
2025-05-04 14:47:37 +0300

    neo-tree copy relative path

---
+++ .config/lazyvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = function(_, opts)
+      opts.window = opts.window or {}
+      opts.window.mappings = opts.window.mappings or {}
+
+      opts.window.mappings["Y"] = function(state)
+        local node = state.tree:get_node()
+        local path = node and node.path or nil
+        if not path then
+          vim.notify("No path to copy", vim.log.levels.WARN)
+          return
+        end
+
+        local cwd = state.path or vim.fn.getcwd()
+        local rel_path = vim.fn.fnamemodify(path, ":.")
+        vim.fn.setreg("+", rel_path)
+        vim.notify("Copied relative path: " .. rel_path)
+      end
+    end,
+  },
+}
