2a1c10c8226ce3ef6d1528e42f9d0f05ba975eba
alidevs <alialatyah2@gmail.com>
2025-08-06 15:07:43 +0300

    add fidget plugin

---
+++ .config/lazyvim/lua/plugins/fidget.lua
@@ -0,0 +1,20 @@
+return {
+  "j-hui/fidget.nvim",
+  opts = {
+    progress = {
+      poll_rate = 10, -- Adjust the poll rate as needed
+      suppress_on_insert = false,
+      ignore_done_already = false,
+      ignore_empty_message = false,
+    },
+    notification = {
+      poll_rate = 10,
+      filter = nil,
+      history_size = 128,
+      override_vim_notify = true,
+      window = {
+        winblend = 0,
+      },
+    },
+  },
+}
