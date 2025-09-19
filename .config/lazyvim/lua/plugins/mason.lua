13238d19d20760d570bc602de7c9a3ec68dee062
alidevs <alialatyah2@gmail.com>
2025-09-19 16:41:42 +0300

    update lazyvim

--- .config/lazyvim/lua/plugins/mason.lua
+++ .config/lazyvim/lua/plugins/mason.lua
@@ -1,14 +1,14 @@
 return {
   {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
         "ruff",
         "biome",
         "pint",
         "prettier",
         "yamlfix",
-      })
-    end,
+      },
+    },
   }
 }
--- .config/lazyvim/lua/plugins/treesitter.lua
+++ .config/lazyvim/lua/plugins/treesitter.lua
@@ -1,34 +1,23 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    vim.list_extend(opts.ensure_installed, {
-      "css",
-      "http",
-      "markdown",
-      "php",
-      "phpdoc",
-      "rust",
-    })
-  end,
-
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-
-    ---@class parser_config
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-    parser_config.blade = {
-      install_info = {
-        url = "https://github.com/EmranMR/tree-sitter-blade",
-        files = { "src/parser.c" },
-        branch = "main",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
       },
-      filetype = "blade",
-    }
-
-    vim.filetype.add({
-      pattern = {
-        [".*%.blade%.php"] = "blade",
-      },
-    })
-  end,
+    },
+  },
 }
