32f4d2b465e59b24be3f0bb87ab662dcb8021a25
alidevs <alialatyah2@gmail.com>
2025-11-13 02:16:27 +0300

    update laravel nvim plugin

--- .config/lazyvim/lua/plugins/laravel.lua
+++ .config/lazyvim/lua/plugins/laravel.lua
@@ -1,47 +1,75 @@
 return {
-  {
-    "adalessa/laravel.nvim",
-    tag = "v3.3.0",
-    dependencies = {
-      "tpope/vim-dotenv",
-      "MunifTanjim/nui.nvim",
-      "kevinhwang91/promise-async",
-      "nvim-neotest/nvim-nio",
+  "adalessa/laravel.nvim",
+  dependencies = {
+    "tpope/vim-dotenv",
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-neotest/nvim-nio",
+    "ravitemer/mcphub.nvim", -- optional
+  },
+  cmd = { "Laravel" },
+  keys = {
+    { "<leader>ll", function() Laravel.pickers.laravel() end,           desc = "Laravel: Open Laravel Picker" },
+    { "<c-g>",      function() Laravel.commands.run("view:finder") end, desc = "Laravel: Open View Finder" },
+    { "<leader>la", function() Laravel.pickers.artisan() end,           desc = "Laravel: Open Artisan Picker" },
+    { "<leader>lt", function() Laravel.commands.run("actions") end,     desc = "Laravel: Open Actions Picker" },
+    { "<leader>lr", function() Laravel.pickers.routes() end,            desc = "Laravel: Open Routes Picker" },
+    -- { "<leader>lh", function() Laravel.run("artisan docs") end,            desc = "Laravel: Open Documentation" },
+    { "<leader>lm", function() Laravel.pickers.make() end,              desc = "Laravel: Open Make Picker" },
+    { "<leader>lc", function() Laravel.pickers.commands() end,          desc = "Laravel: Open Commands Picker" },
+    { "<leader>lo", function() Laravel.pickers.resources() end,         desc = "Laravel: Open Resources Picker" },
+    -- { "<leader>lp", function() Laravel.commands.run("command_center") end, desc = "Laravel: Open Command Center" },
+    {
+      "gf",
+      function()
+        local ok, res = pcall(function()
+          if Laravel.app("gf").cursorOnResource() then
+            return "<cmd>lua Laravel.commands.run('gf')<cr>"
+          end
+        end)
+        if not ok or not res then
+          return "gf"
+        end
+        return res
+      end,
+      expr = true,
+      noremap = true,
     },
-    cmd = { "Laravel" },
-    event = { "VeryLazy" },
-    opts = {},
-    config = function()
-      require("laravel").setup {
-        features = {
-          pickers = {
-            enable = true,
-            provider = 'snacks'
+  },
+  event = { "VeryLazy" },
+
+  opts = {
+    features = {
+      pickers = {
+        provider = "snacks",
+      },
+      route_info = {
+        enable = true,
+      },
+    },
+  },
+
+  config = function(_, opts)
+    local default_envs = require("laravel.options.environments")
+
+    opts.environments = opts.environments or {}
+    opts.environments.env_variable = opts.environments.env_variable or default_envs.env_variable
+    opts.environments.default = opts.environments.default or default_envs.default
+
+    opts.environments.definitions = vim.list_extend(
+      default_envs.definitions,
+      {
+        {
+          name = "custom",
+          map = {
+            php = { "docker", "compose", "run", "--rm", "php" },
+            composer = { "docker", "compose", "run", "--rm", "composer" },
+            npm = { "docker", "compose", "run", "--rm", "node", "npm" },
           },
-          route_info = {
-            enable = true,
-          }
-        },
-        environments = {
-          definitions = {
-            {
-              name = "custom",
-              condition = {
-                callback = function()
-                  return vim.fn.executable("docker") == 1 and vim.fn.filereadable("docker-compose.yml") == 1
-                end
-              },
-              commands = {
-                php = { "docker", "compose", "run", "--rm", "php", "php" },
-                artisan = { "docker", "compose", "run", "--rm", "artisan" },
-                composer = { "docker", "compose", "run", "--rm", "composer" },
-              },
-            },
-          },
-          default = "local",
-          auto_discover = true,
         },
       }
-    end
-  },
+    )
+
+    require("laravel").setup(opts)
+  end
 }
