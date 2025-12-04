f283bd3ab12deb6894f412cf8449c2d58c811c3c
alidevs <alialatyah2@gmail.com>
2025-12-05 01:23:11 +0300

    autocmds to only show cursor in active split

--- .config/lazyvim/lua/config/autocmds.lua
+++ .config/lazyvim/lua/config/autocmds.lua
@@ -6,3 +6,20 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+
+-- show cursorline only in active window enable
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+  group = vim.api.nvim_create_augroup("active_cursorline", { clear = true }),
+  callback = function()
+    vim.opt_local.cursorline = true
+  end,
+})
+
+-- show cursorline only in active window disable
+vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
+  group = "active_cursorline",
+  callback = function()
+    vim.opt_local.cursorline = false
+  end,
+})
