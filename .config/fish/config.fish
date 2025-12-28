df152a815227bbe458ee6d51f0d12f4559cae205
alidevs <alialatyah2@gmail.com>
2025-12-28 22:30:06 +0300

    autocmd to fix dotnet :)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
-and not set -q TMUX
+    and not set -q TMUX
     if tmux has-session -t home
-    exec tmux attach-session -t home
+        exec tmux attach-session -t home
     else
         tmux new-session -s home
     end
@@ -34,15 +34,19 @@ alias up="docker compose up"
 alias build="docker compose build"
 alias down="docker compose down"
 alias dps="docker ps --format 'table {{.ID}}\t{{.Names}}\t{{.Ports}}'"
+alias dn="dotnet"
 
 fish_config theme choose Rosé\ Pine
+set -Ux DOTNET_ROOT /usr/share/dotnet
+fish_add_path /usr/share/dotnet
+fish_add_path $HOME/.dotnet/tools
 
 function fish_user_key_bindings
-  fish_vi_key_bindings
+    fish_vi_key_bindings
 
-  bind -M insert \cs pet-select
-  bind -M insert \eq push-line
-  bind yy fish_clipboard_copy
-  bind Y fish_clipboard_copy
-  bind p fish_clipboard_paste
+    bind -M insert \cs pet-select
+    bind -M insert \eq push-line
+    bind yy fish_clipboard_copy
+    bind Y fish_clipboard_copy
+    bind p fish_clipboard_paste
 end
--- .config/lazyvim/lua/config/autocmds.lua
+++ .config/lazyvim/lua/config/autocmds.lua
@@ -7,7 +7,6 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
-
 -- show cursorline only in active window enable
 vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
   group = vim.api.nvim_create_augroup("active_cursorline", { clear = true }),
@@ -21,5 +20,23 @@ vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
   group = "active_cursorline",
   callback = function()
     vim.opt_local.cursorline = false
+  end,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local ft = vim.bo[args.buf].filetype
+    if ft ~= "cs" and ft ~= "razor" then
+      return
+    end
+
+    -- remove whatever set gd (FzfLua, etc.) for THIS buffer
+    pcall(vim.keymap.del, "n", "gd", { buffer = args.buf })
+
+    -- set native LSP definition (works for Roslyn)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {
+      buffer = args.buf,
+      desc = "Goto Definition (native LSP)",
+    })
   end,
 })
